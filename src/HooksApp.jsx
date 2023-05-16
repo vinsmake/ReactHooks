ea2f7dd029e1556e93ce940db96d8bc1f4ebab22
vinsmake <ps.enrique.e@gmail.com>
2023-05-15 20:44:06 -0600

    chore |Added useState counter app.

---
+++ src/HooksApp.jsx
@@ -0,0 +1,14 @@
+import { CounterApp } from "./HooksList/useState/counterApp"
+
+
+
+export const HooksApp = () => {
+    return (
+      <>
+          <main className="body">
+            <h1>Hooks Recopilation by Vinsmake</h1>
+            <CounterApp></CounterApp>
+          </main>
+      </>
+    )
+}
